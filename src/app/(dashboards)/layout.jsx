31dd3b6e7f293c7efcd15d4b6c5c033e92b8bb1f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-02-08 13:53:11 +0600

    added the new dashboard

---
+++ src/app/(dashboards)/layout.jsx
@@ -0,0 +1,15 @@
+import React, { Children } from "react";
+
+export default function DashboardLayout({ Children }) {
+  return (
+    <div>
+      <h2>Dashboard Layout</h2>
+      <div className="grid grid-cols-12">
+        {/* Nav Site */}
+        <div className="col-span-3"></div>
+        {/* Dashboard Content */}
+        <div className="col-span-9">{Children}</div>
+      </div>
+    </div>
+  );
+}
