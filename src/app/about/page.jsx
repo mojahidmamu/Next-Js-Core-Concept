cd8a642ecbd64ac7505e9dac7fc29e909039a9c5
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-02-08 10:03:59 +0600

    linking and nagigation

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,23 @@
+"use client"
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 export default function AboutPage() {
+    const router = useRouter();
+    const isLoggedIn = true;
+    const handleNavigation = () => {
+        if(isLoggedIn){
+            router.push('/about/address')
+        } else {
+            router.push('/')
+        }
+    }
   return (
     <div>
+      <Link href='/about/address'><li>Address Page</li></Link>
       <p className="text-4xl">About-Page</p>
+      <button type="button" onClick={handleNavigation}>About Page</button>
     </div>
   );
 }
--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 export default function NavBar() {
@@ -5,9 +6,10 @@ export default function NavBar() {
     <div>
       <nav className="flex justify-center">
         <ul className="flex justify-between w-1/2 text-red-400">
-          <li>Home </li>
-          <li>Service</li>
-          <li>About</li>
+          <Link href='/'> <li>Home </li></Link>
+          <Link href='/services'> <li>Services </li></Link>
+          <Link href='/about'> <li>About </li></Link>
+           
         </ul>
       </nav>
     </div>
