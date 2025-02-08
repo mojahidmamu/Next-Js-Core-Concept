f2980133c2bd263880abd1643d74f37452cf964c
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-02-08 15:28:40 +0600

    added new something

--- src/app/(dashboards)/layout.jsx
+++ src/app/(dashboards)/layout.jsx
@@ -3,16 +3,21 @@ import React, { Children } from "react";
 export default function DashboardLayout({ Children }) {
   return (
     <div>
-      <h2>Dashboard Layout</h2>
-      <div className="grid grid-cols-12 ">
+      <h2 className="font-bold text-5xl py-5">Dashboard Layout</h2>
+      <div className="grid grid-cols-12 p-10">
         {/* Nav Site */}
         <div className="col-span-3">
-           <ul> 
-             <li>Home</li>
-           </ul>   
+          <ul>
+            <li>List</li>
+          </ul>
         </div>
         {/* Dashboard Content */}
-        <div className="col-span-9">{Children}</div>
+        <div className="col-span-9 ">
+          <ul>
+            <li>Details</li>
+          </ul>
+          {Children}
+        </div>
       </div>
     </div>
   );
--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,7 +1,15 @@
+"use client"
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 export default function NavBar() {
+  const pathname = usePathname();
+  console.log(pathname, pathname.includes("dashboard"))
+  if(!pathname.includes("dashboard"))
+  {
+    
+  }
   return (
     <div>
       <nav className="flex justify-center">
