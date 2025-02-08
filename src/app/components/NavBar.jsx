13ad683dfa40b5525d1b4b4ffec6d7301e4c35f8
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2025-02-08 17:26:11 +0600

    added

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -8,21 +8,23 @@ export default function NavBar() {
   console.log(pathname, pathname.includes("dashboard"))
   if(!pathname.includes("dashboard"))
   {
-    
+    return (
+      <div>
+        <nav className="flex justify-center">
+          <ul className="flex justify-between w-1/2 text-red-400">
+            <Link href='/'> <li>Home </li></Link>
+            <Link href='/services'> <li>Services </li></Link>
+            <Link href='/about'> <li>About </li></Link>
+            {/* <Link href='/dashboards'> <li>Dashboard </li></Link> */}
+            <Link href='/login'> <li>Login </li></Link>
+            <Link href='/register'> <li>Register </li></Link>
+             
+          </ul>
+        </nav>
+      </div>
+    );
+  } else {
+    return <></>
   }
-  return (
-    <div>
-      <nav className="flex justify-center">
-        <ul className="flex justify-between w-1/2 text-red-400">
-          <Link href='/'> <li>Home </li></Link>
-          <Link href='/services'> <li>Services </li></Link>
-          <Link href='/about'> <li>About </li></Link>
-          {/* <Link href='/dashboards'> <li>Dashboard </li></Link> */}
-          <Link href='/login'> <li>Login </li></Link>
-          <Link href='/register'> <li>Register </li></Link>
-           
-        </ul>
-      </nav>
-    </div>
-  );
+ 
 }
